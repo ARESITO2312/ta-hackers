e6612ca7036f2964f0416de1cd56e7a98fa94951
ARESITO2312 <92240078+ARESITO2312@users.noreply.github.com>
2024-08-07 06:15:38 -0700

    Actualizar programs.rb

--- lib/hackers/dataset/programs.rb
+++ lib/hackers/dataset/programs.rb
@@ -1,180 +1,73 @@
 # frozen_string_literal: true
 
-module Hackers
-  module Programs
-    ##
-    # Program
-    class Program
-      attr_reader :id, :type, :level, :timer
+class Autohack < Sandbox::Script
+  BLACKLIST = [127]
+  TIMEOUT = 300
 
-      attr_accessor :amount
+  def main
+    if @args[0].nil?
+      @logger.log('Specify the number of hosts')
+      return
+    end
 
-      def initialize(api, programs, id = 0, type = 0, level = 0, amount= 0, timer = 0)
-        @api = api
-        @programs = api
-        @id = id
-        @type = type
-        @level = level
-        @amount = amount
-        @timer = timer
+    unless @game.connected?
+      @logger.log(NOT_CONNECTED)
+      return
+    end
+
+    n = 0
+    @game.world.load
+    targets = @game.world.targets
+    @logger.log("Loaded #{targets.count} targets")
+
+    @game = Hackers::Game.new(@game.world)
+
+    loop do
+      targets.each do |target|
+        k = (link unavailable)
+        @logger.log("Target ID: #{k}")
+
+        next if BLACKLIST.include?(k)
+        next if target.nil? || (link unavailable).nil?
+
+        @logger.log("Attacking target ID: #{k}")
+        @logger.log("Attack #{k} / #{target.name}")
+
+        begin
+          net = @game.cmd('NetGetForAttack', target_id: k)
+          @logger.log("Got net for attack")
+
+          # Deja el ion cannon en la toma de red
+          @logger.log("Leaving ion cannon on target ID: #{k}")
+          @game.cmdFight(k, { nodes: 'ion_cannon' })
+          @game.cmdNetLeave(k)
+
+          @logger.log("Left network")
+
+          n += 1
+          @logger.log("Attack count: #{n}")
+
+          return if n == @args[0].to_i
+          return if Time.now - @start_time > TIMEOUT
+
+          sleep(rand(15..25))
+        rescue => e
+          @logger.error(e)
+          @logger.log("Error attacking target ID: #{k}")
+          sleep(rand(165..295))
+        end
       end
 
-      def upgrade
-        @api.upgrade_program(@id)
+      begin
+        targets.new
+      rescue Hackers::RequestError => e
+        if e.type == 'Net::ReadTimeout'
+          @logger.error('Get new targets timeout')
+          retry
+        end
+        @logger.error("Get new targets (#{e})")
+        return
       end
-
-      def finish
-        @api.finish_program(@id)
-      end
-
-      def parse(data)
-        @id = data[0].to_i
-        @type = data[2].to_i
-        @level = data[3].to_i
-        @amount = data[4].to_i
-        @timer = data[5].to_i
-      end
-    end
-
-    ##
-    # Offensive
-    class Offensive < Program
-    end
-
-    ##
-    # Defensive
-    class Defensive < Program
-    end
-
-    ##
-    # Stealth
-    class Stealth < Program
-    end
-
-    ##
-    # AI
-    class AI < Program
-      def revive
-        raw_data = @api.revive_ai(@id)
-        data = Serializer.parseData(raw_data)
-
-        @programs.parse(data[0])
-      end
-    end
-
-    ##
-    # Node
-    class Node < Program
-    end
-
-    ##
-    # Ion Canon
-    class IonCanon < Offensive
-    end
-
-    ##
-    # Shuriken
-    class Shuriken < Offensive
-    end
-
-    ##
-    # Worms
-    class Worms < Offensive
-    end
-
-    ##
-    # Blaster
-    class Blaster < Offensive
-    end
-
-    ##
-    # Shock
-    class Shock < Offensive
-    end
-
-    ##
-    # Battering Ram
-    class BatteringRam < Offensive
-    end
-
-    ##
-    # Maniac
-    class Maniac < Offensive
-    end
-
-    ##
-    # Kraken
-    class Kraken < Offensive
-    end
-
-    ##
-    # Ice Wall
-    class IceWall < Defensive
-    end
-
-    ##
-    # Protector
-    class Protector < Defensive
-    end
-
-    ##
-    # Data Leech
-    class DataLeech < Stealth
-    end
-
-    ##
-    # Access
-    class Access < Stealth
-    end
-
-    ##
-    # Portal
-    class Portal < Stealth
-    end
-
-    ##
-    # Wraith
-    class Wraith < Stealth
-    end
-
-    ##
-    # AI Offensive
-    class AIOffensive < AI
-    end
-
-    ##
-    # AI Defensive
-    class AIDefensive < AI
-    end
-
-    ##
-    # AI Stealth
-    class AIStealth < AI
-    end
-
-    ##
-    # Sentry
-    class Sentry < Node
-    end
-
-    ##
-    # Black Ice
-    class BlackIce < Node
-    end
-
-    ##
-    # Guardian
-    class Guardian < Node
-    end
-
-    ##
-    # Code Gate
-    class CodeGate < Node
-    end
-
-    ##
-    # Turret
-    class Turret < Node
     end
   end
 end
