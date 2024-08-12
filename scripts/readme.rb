89058bcc7fb5b26a2e760a32d76dcaa752f86c5a
ARESITO2312 <92240078+ARESITO2312@users.noreply.github.com>
2024-08-12 10:01:35 -0700

    Actualizar readme.rb

--- scripts/readme.rb
+++ scripts/readme.rb
@@ -18,16 +18,17 @@ class Readme < Sandbox::Script
           @logger.log('Readme is empty')
           return
         end
+
         readme.each do |message|
           @logger.log(message)
         end
       else
         # Modo escritura
         message = @args[1]
-        friend.write_readme(message)
+        @game.player.readme.write(message)
+        @game.player.readme.update
         @logger.log("Mensaje escrito en el readme del jugador #{id}: #{message}")
       end
-
     rescue Hackers::RequestError => e
       @logger.error(e)
     end
