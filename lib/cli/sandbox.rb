6b608f39c2e2ef0151eacdabb05c863633e0b4c3
ARESITO2312 <92240078+ARESITO2312@users.noreply.github.com>
2026-07-25 10:31:47 -0700

    Actualizar sandbox.rb

--- lib/cli/sandbox.rb
+++ lib/cli/sandbox.rb
@@ -1,86 +1,92 @@
 # frozen_string_literal: true
 
-require 'json'
-require 'cli/colorterm'
+require 'io/console'
+require 'rbconfig'
+require_relative 'colorterm'
 
 module Sandbox
-  ##
-  # Config
-  class Config < Hash
-    attr_accessor :file
-
-    def initialize(file)
-      super
-      @file = file
-    end
-
-    def load
-      data = JSON.parse(File.read(@file))
-      return unless data.instance_of?(Hash)
-
-      merge!(data)
-    end
-
-    def save
-      File.write(@file, JSON.pretty_generate(self))
+  # Solo agregamos el método que falta a la clase que ya existe
+  class Config
+    def key?(key)
+      @data&.key?(key.to_s) || false
     end
   end
 
-  ##
-  # Logger
   class Logger
-    attr_accessor :log_cterm, :error_cterm, :info_cterm,
-                  :log_prefix_cterm, :error_prefix_cterm, :info_prefix_cterm,
-                  :log_prefix, :error_prefix, :info_prefix
+    attr_accessor :logPrefix, :logPrefixCterm, :logCterm, :logSuffix
+    attr_accessor :errorPrefix, :errorPrefixCterm, :errorCterm, :errorSuffix
+    attr_accessor :infoPrefix, :infoPrefixCterm, :infoCterm, :infoSuffix
 
     def initialize(shell)
       @shell = shell
-      @log_cterm = ColorTerm.white
-      @error_cterm = ColorTerm.white
-      @info_cterm = ColorTerm.white
-      @log_prefix_cterm = ColorTerm.white
-      @error_prefix_cterm = ColorTerm.white
-      @info_prefix_cterm = ColorTerm.white
-      @log_prefix = String.new
-      @error_prefix = String.new
-      @info_prefix = String.new
     end
 
     def log(message)
-      @shell.puts(@log_prefix_cterm.get(@log_prefix) + @log_cterm.get(message.to_s))
+      line = +""
+      line << "#{logPrefixCterm}#{logPrefix}#{ColorTerm.reset}#{logCterm}#{message}#{ColorTerm.reset}#{logSuffix}"
+      @shell.puts(line)
+      begin
+        if defined?(Readline)&.respond_to?(:refresh_line)
+          Readline.refresh_line if @reading
+        elsif defined?(Reline)&.respond_to?(:refresh_line)
+          Reline.refresh_line if @reading
+        end
+      rescue StandardError
+        nil
+      end
     end
 
     def error(message)
-      @shell.puts(@error_prefix_cterm.get(@error_prefix) + @error_cterm.get(message.to_s))
+      line = +""
+      line << "#{errorPrefixCterm}#{errorPrefix}#{ColorTerm.reset}#{errorCterm}#{message}#{ColorTerm.reset}#{errorSuffix}"
+      @shell.puts(line)
+      begin
+        if defined?(Readline)&.respond_to?(:refresh_line)
+          Readline.refresh_line if @reading
+        elsif defined?(Reline)&.respond_to?(:refresh_line)
+          Reline.refresh_line if @reading
+        end
+      rescue StandardError
+        nil
+      end
     end
 
     def info(message)
-      @shell.puts(@info_prefix_cterm.get(@info_prefix) + @info_cterm.get(message.to_s))
+      line = +""
+      line << "#{infoPrefixCterm}#{infoPrefix}#{ColorTerm.reset}#{infoCterm}#{message}#{ColorTerm.reset}#{infoSuffix}"
+      @shell.puts(line)
+      begin
+        if defined?(Readline)&.respond_to?(:refresh_line)
+          Readline.refresh_line if @reading
+        elsif defined?(Reline)&.respond_to?(:refresh_line)
+          Reline.refresh_line if @reading
+        end
+      rescue StandardError
+        nil
+      end
     end
   end
 
-  ##
-  # Parent class for scripts
-  class Script
-    ##
-    # Creates new script:
-    #   game    = Game
-    #   shell   = Shell
-    #   logger  = Logger
-    #   args    = Arguments
-    def initialize(game, shell, logger, args)
-      @game = game
-      @shell = shell
-      @logger = logger
-      @args = args
+  class Shell
+    def initialize(input = $stdin, output = $stdout)
+      @input = input
+      @output = output
+      @reading = false
     end
 
-    ##
-    # Script entry point
-    def main; end
+    def puts(*args)
+      args.each { |line| @output.puts(line) }
+    end
 
-    ##
-    # Executes after the script finished
-    def finish; end
+    def print(*args)
+      @output.print(*args)
+    end
+
+    def readline(prompt = "", add_history = false)
+      @reading = true
+      line = Readline.readline(prompt, add_history)
+      @reading = false
+      line
+    end
   end
 end
