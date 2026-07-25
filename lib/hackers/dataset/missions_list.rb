7b76e820de757578d824f3dee1a630349a74ccd3
ARESITO2312 <92240078+ARESITO2312@users.noreply.github.com>
2026-07-25 07:36:55 -0700

    Actualizar missions_list.rb

--- lib/hackers/dataset/missions_list.rb
+++ lib/hackers/dataset/missions_list.rb
@@ -30,7 +30,6 @@ module Hackers
 
     def initialize(*)
       super
-
       @missions = []
     end
 
@@ -55,28 +54,35 @@ module Hackers
 
     def parse
       data = Serializer.parseData(@raw_data)
-
       @missions.clear
+
+      # Solo procesa si hay datos válidos
+      return unless data && data[0]
+
       data[0].each do |record|
+        # Valor por defecto vacío si no existe el campo
+        goals = record[4] ? Serializer.normalizeData(record[4]).split(',') : []
+        req_missions = record[9] ? Serializer.normalizeData(record[9]).split(',') : []
+
         @missions << Mission.new(
-          record[0].to_i,
+          record[0]&.to_i || 0,
           record[1],
           Serializer.normalizeData(record[2]),
           record[3],
-          Serializer.normalizeData(record[4]).split(','),
-          record[5].to_i,
-          record[6].to_i,
-          record[7].to_i,
-          Serializer.normalizeData(record[9]).split(','),
-          record[12].to_i,
-          record[13].to_i,
-          record[14].to_i,
-          Serializer.normalizeData(record[17]),
-          Serializer.normalizeData(record[19]),
-          Serializer.normalizeData(record[21]),
-          Serializer.normalizeData(record[22]),
-          record[24].to_i,
-          record[25].to_i,
+          goals,
+          record[5]&.to_i || 0,
+          record[6]&.to_i || 0,
+          record[7]&.to_i || 0,
+          req_missions,
+          record[12]&.to_i || 0,
+          record[13]&.to_i || 0,
+          record[14]&.to_i || 0,
+          Serializer.normalizeData(record[17] || ''),
+          Serializer.normalizeData(record[19] || ''),
+          Serializer.normalizeData(record[21] || ''),
+          Serializer.normalizeData(record[22] || ''),
+          record[24]&.to_i || 0,
+          record[25]&.to_i || 0,
           record[28]
         )
       end
